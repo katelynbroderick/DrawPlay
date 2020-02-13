ef9a1a1abd224d10ebbb6389c1e9f193b036eee1
Heidi Chen <hchen2@mymail.mines.edu>
2020-02-13 13:06:56 -0700

    made mouth bigger

--- src/Cat.java
+++ src/Cat.java
@@ -44,7 +44,7 @@ public class Cat {
 		g2.setColor(Color.pink);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
-		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT+50);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
